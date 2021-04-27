5f4ba6ac78164c9674ea78eb6edc93681347c91a
Adam Cecile <acecile@le-vert.net>
2021-04-27 23:07:10 +0200

    Quick n diry pandas/matplotlib to generate number of posts per day line chart

---
+++ plot.py
@@ -0,0 +1,16 @@
+import pandas
+import matplotlib.pyplot
+
+df = pandas.read_excel("./posts_parsed.xlsx")
+df.columns = ["author", "timestamp"]
+df.timestamp = pandas.to_datetime(df.timestamp)
+df["date"] = df.timestamp.dt.date
+
+print(df.head())
+print(df.info())
+
+by_date = df.groupby("date").size()
+print(by_date)
+by_date.plot()
+
+matplotlib.pyplot.show()
